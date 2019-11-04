992f3fab42a59b511afbcc705a04e2923c627293
Rahul Gupta <rahul.gupta2@cbre.com>
2019-11-04 17:23:41 +0530

    Created Project

---
+++ TestPipeLine/TestPipeLine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestPipeLine
+//
+//  Created by Gupta, Rahul @ Gurgaon on 04/11/19.
+//  Copyright © 2019 Gupta, Rahul @ Gurgaon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestPipeLine/TestPipeLineTests/TestPipeLineTests.swift
@@ -0,0 +1,34 @@
+//
+//  TestPipeLineTests.swift
+//  TestPipeLineTests
+//
+//  Created by Gupta, Rahul @ Gurgaon on 04/11/19.
+//  Copyright © 2019 Gupta, Rahul @ Gurgaon. All rights reserved.
+//
+
+import XCTest
+@testable import TestPipeLine
+
+class TestPipeLineTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
